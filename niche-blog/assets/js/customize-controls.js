37fef3aa4a5c1bedd9c26bfeded67303c77e7bf7
Sean Murphy <sean@iamseanmurphy.com>
2024-10-14 01:09:05 +0000

    0.1.0

---
+++ niche-blog/assets/js/customize-controls.js
@@ -0,0 +1,22 @@
+/**
+ * File customize-controls.js.
+ *
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+
+( function( $, api ) {
+
+	/* === Upsell Section === */
+	api.sectionConstructor['niche-blog-upsell'] = api.Section.extend( {
+
+		// No events for this type of section.
+		attachEvents: function () {},
+
+		// Always make the section active.
+		isContextuallyActive: function () {
+			return true;
+		}
+	} );
+} )( jQuery, wp.customize );
